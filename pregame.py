a0406ed8bd382f67d11e6462ec2b3ba5dc906775
PatrickKFoley <patrickkyefoley@gmail.com>
2023-08-11 00:01:38 -0400

    server fix 2

--- pregame.py
+++ pregame.py
@@ -1,8 +1,3 @@
-from pygame.locals import *
-from network import Network
-from request import Request
-from network import Network
-
 class Pregame:
     def __init__(self, id):
         self.id = id
