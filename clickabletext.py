dcc32e59feba8321268fd3b7ec01fa6e326d1cb5
PatrickKFoley <patrickkyefoley@gmail.com>
2024-04-15 11:53:07 -0400

    growing text on hover

--- clickabletext.py
+++ clickabletext.py
@@ -1,33 +1,51 @@
 import pygame
 
+MAX_GROWTH = 10
 
 class ClickableText:
     def __init__(self, text, size, x, y):
         self.y = y
         self.x = x
-        self.text_unselected, self.text_selected, self.rect = self.createText(text, size)
-        self.rect.center = [x, y]
+        self.text_unselected, self.text_selected, self.rect, small = self.createText(text, size)
+        self.center = [x, y]
+        self.rect.center = self.center
 
-        self.surface = self.text_unselected
+        self.length = small.get_size()[0]
+        self.width = small.get_size()[1]
+
+        self.length_width_multiplier = int(self.length/self.width)
+        self.growth_amount = 0
+
+        self.buildSurface()
 
     @staticmethod
     def createText(text, size):
         font_small = pygame.font.Font("backgrounds/font.ttf", size)
         font_large = pygame.font.Font("backgrounds/font.ttf", size + 10)
 
-        text_unselected = font_small.render(text, True, "white")
+        text_small = font_small.render(text, True, "white")
+        text_unselected = font_large.render(text, True, "white")
         text_selected = font_large.render(text, True, "lightgray")
         text_rect = text_unselected.get_rect()
 
-        return text_unselected, text_selected, text_rect
+        return text_unselected, text_selected, text_rect, text_small
+    
+    def buildSurface(self, hover = False):
+        if hover:
+            self.surface = pygame.transform.smoothscale(self.text_selected, (self.length + self.growth_amount*self.length_width_multiplier, self.width + self.growth_amount))
+        else:
+            self.surface = pygame.transform.smoothscale(self.text_unselected, (self.length + self.growth_amount*self.length_width_multiplier, self.width + self.growth_amount))
+
+        self.rect = self.surface.get_rect()
+        self.rect.center = self.center
 
     def update(self, mouse_pos):
         if self.rect.collidepoint(mouse_pos):
-            self.surface = self.text_selected
-            self.rect = self.surface.get_rect()
-            self.rect.center = [self.x, self.y]
+            if self.growth_amount < MAX_GROWTH:
+                self.growth_amount += 1
+                self.buildSurface(True)
 
         else:
-            self.surface = self.text_unselected
-            self.rect = self.surface.get_rect()
-            self.rect.center = [self.x, self.y]
+            if self.growth_amount > 0:
+                self.growth_amount -= 1
+                self.buildSurface()
